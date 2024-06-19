40ac4c34acc53d8633e9b6aebf95b7b38f81dcc2
Jorge Herrera Robledo <jherrera@makita.cl>
2024-06-19 15:49:30 -0400

    se agrega endpoint qure consulta las ordenes de servicio por rut del servicio tecnico

--- controllers/obtenerOrdenServicioControllers.js
+++ controllers/obtenerOrdenServicioControllers.js
@@ -1,5 +1,8 @@
 const axios = require('axios');
 const logger = require('../config/logger.js');
+const { connectToDatabase, closeDatabaseConnection } = require('../config/database.js');
+const sql = require('mssql');
+require('dotenv').config();
 
 async function obtenerOrdenServicio(req, res) {
     try {
@@ -58,6 +61,94 @@ async function obtenerOrdenServicio(req, res) {
     }
 }
 
+/**
+ * Se obtienen ordenes de servicio utilizando rut de la entidad del servicio tecnico
+ * @param {*} entidadesList 
+ * @returns 
+ */
+async function obtenerOrdenesRut(req, res) {
+    logger.info(`Iniciamos la función obtenerOrdenesPorRut`);
+
+    try {
+        let entidadesList = req.body;
+        const dateInicio = await fechaInicio();
+        const dateFin = await fechaFin();
+        let ordenesPendientesList = [];
+        //const ordenesPendientesList2 = [{Entidad:'76890098-0' , Direccion:'SANTA ROSA1508-1510' }, {Entidad:'76279534-5',Direccion:'SANTA ROSA1508-1510'} , {Entidad:'16205650-8',Direccion:'SANTA ROSA1508-1510'},]; // se deja a modo de prueba
+        for (const entidad of entidadesList) {
+            try {
+                entidad.Entidad = entidad.Entidad.replace(/-/g, '');
+                const url = `http://backend2.telecontrol.com.br/homologation-os/ordem/cnpj/${entidad.Entidad}/dataInicio/${dateInicio}/dataFim/${dateFin}`;
+                logger.info(`URL :  ${url}`);
+
+                const response = await axios.get(url, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Access-Application-Key': '3d137dea1d13220aa9a10ee57d69f6b30d247f28',
+                        'Access-Env': 'HOMOLOGATION',
+                        'X-Custom-Header': 'value'
+                    }
+                });
+
+            if (response.data && response.data.os) {
+                const updatedOs = response.data.os.map(os => ({
+                    ...os,
+                    direccion: entidad.Direccion
+                  }));
+                ordenesPendientesList = ordenesPendientesList.concat(updatedOs);
+            }
+            } catch (error) {
+                if (error.response && error.response.status === 404) {
+                    logger.error(`Error ${JSON.stringify(error.response.data)}`);
+                } else {
+                    logger.error(`Error al procesar la entidad ${entidad.Entidad}: ${error.message}`);
+                    throw error; 
+                }
+            }
+        }
+
+        const filtradas = ordenesPendientesList.filter(orden => orden.descricao_tipo_atendimento === "Puesta En Marcha")
+                                               .map(orden => ({ ...orden, idPedido: null }));;
+
+
+        logger.info(`Fin de la función obtenerOrdenesPorRut`);
+       
+        res.status(200).json(filtradas);
+    
+    } catch (error) {
+        // Manejamos cualquier error ocurrido durante el proceso
+        logger.error(`Error en obtenerOrdenServicioPorRut: ${error.message}`);
+        res.status(500).json({ error: `Error en el servidor [obtener-orden-servicio-ms/obtenerOrdenServicioPorRut] :  ${error.message}`  });
+       
+    }
+}
+
+
+async function  fechaFin(){
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // Los meses son 0-11
+    const day = String(today.getDate()).padStart(2, '0'); // Los días son 1-31
+
+    const formattedDate = `${year}-${month}-${day}`;
+    return formattedDate;
+}
+
+
+async function fechaInicio(){
+    const today = new Date();
+    const back = new Date(today);
+    back.setDate(today.getDate() - 30);
+
+    const year = back.getFullYear();
+    const month = String(back.getMonth() + 1).padStart(2, '0');
+    const day = String(back.getDate()).padStart(2, '0');
+
+    const formattedBackDate = `${year}-${month}-${day}`;
+
+    return formattedBackDate;
+}
+
 module.exports = {
-    obtenerOrdenServicio
+    obtenerOrdenServicio , obtenerOrdenesRut
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { obtenerOrdenServicio } = require('../controllers/obtenerOrdenServicioControllers');
+const { obtenerOrdenServicio , obtenerOrdenesRut } = require('../controllers/obtenerOrdenServicioControllers');
 
 router.post('/obtener-orden-servicio', obtenerOrdenServicio);
+router.post('/obtener-orden-servicio-rut', obtenerOrdenesRut);
 
 module.exports = router;
